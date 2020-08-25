f0f50d92424b4bc6f51963af393b9a6481edbf75
baozhifei <baozhifei@gmail.com>
2020-08-25 23:51:41 +0800

    add demo target

---
+++ libmalloc_demo/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  libmalloc_demo
+//
+//  Created by 鲍志飞 on 2020/8/15.
+//  Copyright (c) 2020 鲍志飞. All rights reserved.
+
+#include <stdlib.h>
+
+int main(int argc, const char * argv[]) {
+	void * p = malloc(1);
+    return 0;
+}
